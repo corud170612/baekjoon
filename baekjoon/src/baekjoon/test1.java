1ecc8bcbc18a42d550ff90ec8dbcd02d30fdae24
jyjeon <jyjeon@DESKTOP-NNU0ROF>
2021-12-16 16:21:42 +0900

    2021.12.16 백준 단계별로 풀어보기 1일

---
+++ baekjoon/src/baekjoon/test1.java
@@ -0,0 +1,12 @@
+package baekjoon;
+//단계별로 풀어보기
+//입출력과 사칙연산
+//2557번
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ baekjoon/src/baekjoon/test2.java
@@ -0,0 +1,13 @@
+package baekjoon;
+//단계별로 풀어보기
+//입출력과 사칙연산
+//10718번
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}
