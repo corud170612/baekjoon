3cae4202adfb9962d1a10ea05ddabc4d34157e8a
jyjeon <jyjeon@DESKTOP-NNU0ROF>
2021-12-16 18:04:08 +0900

    2021.12.16 단계별로 풀어보기 1일차

---
+++ baekjoon/src/baekjoon/test4.java
@@ -0,0 +1,16 @@
+package baekjoon;
+//단계별로 풀어보기
+//입출력과 사칙연산
+//10172번
+public class test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
---
+++ baekjoon/src/baekjoon/test5.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+//단계별로 풀어보기
+//입출력과 사칙연산
+//1000번
+public class test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A + B);
+	}
+
+}
