0d1f7b9bfd1bf9d03dab07916881a7ecd9def9d3
jyjeon <jyjeon@192.168.219.102>
2021-12-22 18:17:11 +0900

    2021.12.22 단계별로 풀어보기 2일차

--- baekjoon/src/baekjoon/test6.java
+++ baekjoon/src/baekjoon/test6.java
@@ -4,24 +4,25 @@ import java.util.Scanner;
 
 //단계별로 풀어보기
 //입출력과 사칙연산
-//1000번
-public class test5 {
+//1001번
+public class test6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//1000번 코드 가져와서 만듬
 //		Scanner sc = new Scanner(System.in);
+//		System.out.println("A 값 입력");
 //		int A = sc.nextInt();
+//		System.out.println("B 값 입력");
 //		int B = sc.nextInt();
-//		
-//		System.out.println(A + B);
-//		처음 블로그 보고 따라하고 입출력값을 직접 디테일하게 보고 싶어 만든 코드임, 2021.12.22
+//		System.out.println("A 값 = "+A+" B 값 = "+B);
+//		System.out.println("A - B 는 = "+(A-B));
+		
+		//백준 제출용
 		Scanner sc = new Scanner(System.in);
-		System.out.println("A 값 입력");
 		int A = sc.nextInt();
-		System.out.println("B 값 입력");
 		int B = sc.nextInt();
-		System.out.println("A 값 = "+A+" B 값 = "+B);
-		System.out.println("A + B 는 = "+(A+B));
+		System.out.println(A-B);
 	}
 
 }
---
+++ baekjoon/src/baekjoon/test7.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+//단계별로 풀어보기
+//입출력과 사칙연산
+//10998번
+public class test7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A*B);
+		
+		
+	}
+
+}
